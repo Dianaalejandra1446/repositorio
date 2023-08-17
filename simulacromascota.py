47f49aeb53736e7e23e644b730ae6ce94c804572
Dianaalejandra1446 <dianaleja04@hotmail.com>
2023-08-16 20:24:26 -0500

    actualizacion

--- simulacromascota.py
+++ simulacromascota.py
@@ -1,16 +1,8 @@
 """Simulacro CICLO 1
-Elabore un programa Python para gestionar el CRUD del archivo de datos PetShopping.json con las siguientes funcionalidades:
-Mostrar en pantalla todas las mascotas a la venta visualizando: Tipo, Raza, Precio y Servicios
-Crear Nueva mascota con la posibilidad de múltiples ítems de Servicio
-Mostrar los datos de Mascotas por Tipo elegido visualizando: Raza, Precio y Servicios
-Actualizar los datos de una mascota consultada por índice (Mostrar el listado total y elegir por índice) 
-Eliminar una mascota de la tienda (Mostrar el listado total y elegir por índice)"""
+ Elabore un programa Python para gestionar el CRUD del archivo de datos PetShopping.json con las siguientes funcionalidades:Mostrar en pantalla todas las mascotas a la venta visualizando: Tipo, Raza, Precio y Servicios Crear Nueva mascota con la posibilidad de múltiples ítems de Servicio 
+ Mostrar los datos de Mascotas por Tipo elegido visualizando: Raza, Precio y Servicios Actualizar los datos de una mascota consultada por índice (Mostrar el listado total y elegir por índice) Eliminar una mascota de la tienda (Mostrar el listado total y elegir por índice)"""
 
 import json
-with open('PetShopping.json','r') as file:
-    data = json.load(file)
-
-datosmascotas = {}
 
 def leerInt(msg):
     while True:
@@ -19,20 +11,21 @@ def leerInt(msg):
             return iNum
         except ValueError:
             print("-"*75)
-            print("Ingrese un numero entero valido ")
+            print("Ingrese un numero entero valido")
+
 
 def leerString(msg):
     while True:
         try:
-            n = int(input(msg))
+            n = input(msg)
             if n.strip() == "":
-                print("Error ingrese un nombre valido! ")
-                input("Digite cualquier tecla para continuar")
+                print("Error! Ingrese el nombre valido.")
+                input("Digite cualquier tecla para continuar...")
                 continue
             return n
         except ValueError as e:
-            print("-"*75)
-            print("Error! Hay que ingresar un nombre. ")
+            print("Error! Hay que ingresar el nombre.")
+
 
 def msgError(msg):
     print("----> ¡ERROR!" + msg)
@@ -53,22 +46,40 @@ def menu():
     if elegirop < 1 or elegirop > 6:
         msgError('Ingrese una opcion valida')
     return elegirop
+
 def elegir():
-    datosmascotas['pets']=[]
     print('     MASCOTAS DISPONIBLES:\n')
-    for elem in data["pets"]:
+    for elem in datosmascotas["pets"]:
         print('_'*30,'\n')
         print('Tipo: ','-'*5,elem["tipo"],'\n')
         print('Raza: ','-'*5,elem["raza"],'\n')
         print('Precio:','-'*5,elem["precio"],'\n')
-        max = len(elem["servicios"])
-        print('Servicios: ')
-        for i in range(0,max):
-            print(f'{i+1}. {elem["servicios"][i]}')
-        print('_'*30,'\n')
+        servicios = ", ".join(elem["servicios"])
+        print(f'Servicios: {servicios}')
     input("---> Presione ENTER para continuar")
-def crear():
-    pass
+
+def crear(datosmascotas):
+    datosmascotas['pets']=[]
+    print('     INGRESAR MASCOTA:\n')
+
+    IngresaTipo = leerString("Ingresar tipo de mascota:")
+    IngresaRaza = leerString("Ingresar tipo de raza: ")
+    IngresaPrecio = leerInt("Ingresar precio mascota: ")
+    IngresaServicio = leerString("Ingrese servicios mascota: ")
+
+    datosmascotas["pets"].append({
+        'tipo':IngresaTipo,
+        'raza':IngresaRaza,
+        'precio':IngresaPrecio,
+        'servicios':IngresaServicio
+    })
+    #datosmascotas.append(["pets"])
+    with open('PetShopping.json','w',encoding="utf-8")as archivo:
+        json.dump(datosmascotas,archivo,indent=4)
+
+    input("---> Presione ENTER para continuar")
+    return datosmascotas
+
 def buscar():
     pass
 def actualizar():
@@ -76,12 +87,19 @@ def actualizar():
 def eliminar():
     pass
 
+
+
+#CODIGO PRINCIPAL
+datosmascotas = {}
+with open('PetShopping.json','r',encoding="utf-8") as file:
+    datosmascotas = json.load(file)
+
 while True:
     elegirop = menu()
     if elegirop == 1:
         elegir()
     elif elegirop == 2:
-        crear()
+        crear(datosmascotas)
     elif elegirop == 3:
         buscar()
     elif elegirop == 4:
